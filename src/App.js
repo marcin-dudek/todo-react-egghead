0efc335d98ed91ae9ce99c0937cc197e94371a48
Marcin Dudek <manekd@gmail.com>
2017-02-05 15:02:40 +0100

    Added binding to list and handling current todo

--- src/App.js
+++ src/App.js
@@ -3,28 +3,51 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Todos</h2>
-        </div>
-        <div className="Todo-App">
-            <form action="">
-                <input type="text"/>
-            </form>
-            <div className="Todo-List">
-                <ul>
-                    <li><input type="checkbox"/>Learn JSX</li>
-                    <li><input type="checkbox"/>Build awesome app</li>
-                    <li><input type="checkbox"/>Ship it!</li>
-                </ul>
+    constructor() {
+        super()
+        this.state = {
+            todos: [
+                { id: 1, name: "Learn JSX", isComplete: true },
+                { id: 2, name: "Build awesome app", isComplete: false },
+                { id: 3, name: "Ship it!", isComplete: false }
+            ],
+
+            currentTodo: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            currentTodo: e.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>React Todos</h2>
+                </div>
+                <div className="Todo-App">
+                    <form action="">
+                        <input type="text" onChange={this.handleInputChange} value={this.state.currentTodo} />
+                    </form>
+                    <div className="Todo-List">
+                        <ul>
+                            {this.state.todos.map(t =>
+                                <li key={t.id}>
+                                    <input type="checkbox" defaultChecked={t.isComplete} />{t.name}
+                                </li>)
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
